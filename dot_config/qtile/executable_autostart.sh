9b21738e2e11b8bcd1bc53b01301f828ec48b55b
ronny <ronny@pop-os.localdomain>
2022-10-27 07:35:12 +0700

    Fix: imwheel m4,5 enable and add missing lib in config

--- dot_config/qtile/executable_autostart.sh
+++ dot_config/qtile/executable_autostart.sh
@@ -5,6 +5,6 @@ picom --config ~/.config/picom/picom.conf & disown
 xrandr --output DP-4 --primary --mode 2560x1440 --rate 74.92
 xrandr --output HDMI-0 --mode 1920x1080 --right-of DP-4
 
-imwheel
+imwheel -b "4 5"
 pasystray &
 
