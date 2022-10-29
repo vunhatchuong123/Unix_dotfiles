e57cdc20b1619813ab8e1487572c18a9c5e5d36a
ronny <ronny@pop-os.localdomain>
2022-10-29 17:43:14 +0700

    Add scratchpad to qtile

---
+++ dot_config/qtile/modules/scratchpad.py
@@ -0,0 +1,39 @@
+from .keys import terminal, keys, mod
+from .groups import groups
+from libqtile.config import ScratchPad, DropDown, Key
+from libqtile.command import lazy
+
+# Configuration
+height =				0.8
+y_position =			0.1
+opacity =				1
+
+groups.append(ScratchPad("scratchpad", [
+    DropDown("term",
+             terminal,
+             height=height,
+             y=y_position,
+             opacity=opacity),
+
+    DropDown("ranger",
+             terminal + " -e ranger",
+             height=height,
+             y=y_position,
+             opacity=opacity),
+
+#	DropDown("music",
+#			terminal + " -e ncmpcpp",
+#            height=height,
+#            y=y_position,
+#            opacity=opacity),
+#
+    #DropDown("volume", "pavucontrol", width=0.8, height=0.8, x=0.1, y=0.1, opacity=0.9),
+
+]))
+
+keys.extend([
+    Key([mod], "i", lazy.group['scratchpad'].dropdown_toggle('term')),
+    Key([mod], "c", lazy.group['scratchpad'].dropdown_toggle('ranger')),
+
+])
+
