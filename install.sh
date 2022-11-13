3f0ed57aa32a4ddca07d407f1a5d4af20bf59e73
ronny <ronnyvu321@gmail.com>
2022-11-13 11:35:50 +0700

    Test install script

--- install.sh
+++ install.sh
@@ -2,11 +2,39 @@
 
 set -e # -e: exit on error
 
-repo="https://github.com/vunhatchuong123/Unix_dotfiles.git"
+log_color() {
+  color_code="$1"
+  shift
+
+  printf "\033[${color_code}m%s\033[0m\n" "$*" >&2
+}
+
+log_red() {
+  log_color "0;31" "$@"
+}
+
+log_blue() {
+  log_color "0;34" "$@"
+}
+
+log_task() {
+  log_blue "🔃" "$@"
+}
+
+log_error() {
+  log_red "❌" "$@"
+}
+
+error() {
+  log_error "$@"
+  exit 1
+}
+
 
 if [ ! "$(command -v chezmoi)" ]; then
   bin_dir="$HOME/.local/bin"
   chezmoi="$bin_dir/chezmoi"
+  log_task "Installing chezmoi to '${chezmoi}'"
   if [ "$(command -v curl)" ]; then
     sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
   elif [ "$(command -v wget)" ]; then
@@ -19,4 +47,9 @@ else
   chezmoi=chezmoi
 fi
 
-exec "$chezmoi" init --apply --verbose --progress $repo
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+
+log_task "Running 'chezmoi init --apply --source=$script_dir"
+# replace current process with chezmoi init
+exec "$chezmoi" init --apply "--source=$script_dir"
